6e0307fa6d1c9afbdb478fe649deee4b425c34df
IrinaSing <irinasingh.mail@yandex.ru>
2021-03-25 22:17:19 +0100

    spell-it-2 comments

--- forest-and-trees/exercises/spell-it-2.js
+++ forest-and-trees/exercises/spell-it-2.js
@@ -1,21 +1,24 @@
-let userInput = '';
+let userInput = "";
 
 let userConfirmed = false;
 while (!userConfirmed) {
-  userInput = prompt('enter a word to spell:');
+  userInput = prompt("enter a word to spell:");
 
-  if (userInput === '' || userInput === null) {
-    alert('nope, enter something');
+  //there is a difference from spell-it-2
+  //When "if" conditions does not return "true", the program goes further to "else" conditions
+  //instead of skipping the rest of the loop body and going back to check the condition
+  if (userInput === "" || userInput === null) {
+    alert("nope, enter something");
   } else {
     userConfirmed = confirm(
-      'do you want to spell this word?\n\n' + '- "' + userInput + '"'
+      "do you want to spell this word?\n\n" + '- "' + userInput + '"'
     );
   }
 }
 
-let spelledOut = 'all spelled out:\n';
+let spelledOut = "all spelled out:\n";
 for (let nextLetter of userInput) {
-  spelledOut = spelledOut + '\n- ' + nextLetter;
+  spelledOut = spelledOut + "\n- " + nextLetter;
 }
 
 alert(spelledOut);
