c8bbab38babba46c201cf469406856b92a9889c5
IrinaSing <irinasingh.mail@yandex.ru>
2021-03-29 23:18:01 +0200

    solved Repeated

--- word-banks/exercises/remove-repeated-characters.js
+++ word-banks/exercises/remove-repeated-characters.js
@@ -1,35 +1,35 @@
 /*
   unused:
+  used:
+    character
+    phrase    
     replace
     includes
-    character
-    phrase
-  used:
 
 */
 
-let phrase = '';
-while (phrase === '' || phrase === null) {
-  phrase = prompt('enter some text');
+let phrase = "";
+while (phrase === "" || phrase === null) {
+  phrase = prompt("enter some text");
 }
 
-let repeated = '';
-let notRepeated = '';
+let repeated = "";
+let notRepeated = "";
 
-for (let _ of _) {
-  if (repeated._(character)) {
+for (let character of phrase) {
+  if (repeated.includes(character)) {
     continue;
   }
-  if (notRepeated._(character)) {
-    notRepeated = notRepeated._(character, '');
-    repeated = repeated + _;
+  if (notRepeated.includes(character)) {
+    notRepeated = notRepeated.replace(character, "");
+    repeated = repeated + character;
   } else {
-    notRepeated = notRepeated + _;
+    notRepeated = notRepeated + character;
   }
 }
 
 alert(
-  '"' + phrase + '"\nrepeated: ' + repeated + '\nnot repeated: ' + notRepeated
+  '"' + phrase + '"\nrepeated: ' + repeated + "\nnot repeated: " + notRepeated
 );
 
 /* -- errors --
