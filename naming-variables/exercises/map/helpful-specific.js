a578f3e70aa58c7d8b066c98c8ea248c55b4b037
IrinaSing <irinasingh.mail@yandex.ru>
2021-04-02 23:28:25 +0200

    special names for map

--- naming-variables/exercises/map/helpful-specific.js
+++ naming-variables/exercises/map/helpful-specific.js
@@ -1,3 +1,32 @@
 /* specific
 
+textForVowelsCapitalization - text, typed by user that will be
+transformed via capitalization of all the vowels.
+
+vowelOrNot - variable that takes each character of user input to compare
+with set of vowels (to determine if the characted has to be capitalized)
+
+outputWithCapitalizedVowels - variable for text in output in final alert. 
+the user input with capitalized vowels will be displayed.
 */
+let textForVowelsCapitalization = null;
+while (textForVowelsCapitalization === null) {
+  textForVowelsCapitalization = prompt(
+    "enter some text, can you guess what the program does?"
+  );
+}
+
+let outputWithCapitalizedVowels = "";
+for (let vowelOrNot of textForVowelsCapitalization) {
+  if ("aeiou".includes(vowelOrNot)) {
+    outputWithCapitalizedVowels =
+      outputWithCapitalizedVowels + vowelOrNot.toUpperCase();
+  } else if ("AEIOU".includes(vowelOrNot)) {
+    outputWithCapitalizedVowels =
+      outputWithCapitalizedVowels + vowelOrNot.toLowerCase();
+  } else {
+    outputWithCapitalizedVowels = outputWithCapitalizedVowels + vowelOrNot;
+  }
+}
+
+alert(outputWithCapitalizedVowels);
