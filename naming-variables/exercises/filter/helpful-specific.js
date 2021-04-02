11d0a439c2473a5b6108a41224ccc372fa47b329
IrinaSing <irinasingh.mail@yandex.ru>
2021-04-02 23:33:50 +0200

    textToFilter

--- naming-variables/exercises/filter/helpful-specific.js
+++ naming-variables/exercises/filter/helpful-specific.js
@@ -1,5 +1,5 @@
 /* specific
-someText - stores user input for it's futher comparison and transformation.
+textToFilter - stores user input for it's futher comparison and transformation.
 
 textWithoutVowels - this variable will appear in the final output
 as a transformed user input (vowels will be removed).
@@ -10,13 +10,13 @@ so that all of them could be compared to vowels one by one.
 vowels - fixed variable that stores set of symbols that will be compared to user input via includes.
 */
 
-let someText = null;
-while (someText === null) {
-  someText = prompt("enter some text, the program will remove all vowels");
+let textToFilter = null;
+while (textToFilter === null) {
+  textToFilter = prompt("enter some text, the program will remove all vowels");
 }
 
 let textWithoutVowels = "";
-for (let character of someText) {
+for (let character of textToFilter) {
   let vowels = "aeiou".includes(character.toLowerCase());
   if (!vowels) {
     textWithoutVowels = textWithoutVowels + character;
