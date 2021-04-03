42243023826ba6fe85bcc60b5f23949184eb5c3f
IrinaSing <irinasingh.mail@yandex.ru>
2021-04-03 23:53:07 +0200

    what does program do

--- naming-variables/exercises/sort-by-length/unhelpful-names.js
+++ naming-variables/exercises/sort-by-length/unhelpful-names.js
@@ -1,28 +1,32 @@
+/* The program requires from user to input strings one by one and sends alerts after every input informing
+the user if the string was short, medium or long. When user wants to stop, it's required to press "cancel".
+After that program will send an alert displaying all strings sorted out by short, medium, long. */
+
 alert(
-  'enter many strings, they will be sorted by length.\n\n' +
+  "enter many strings, they will be sorted by length.\n\n" +
     'when you are done entering strings you can "cancel" or "escape"'
 );
 
-let _a = 'short:';
-let _b = 'medium:';
-let _c = 'long:';
+let _a = "short:";
+let _b = "medium:";
+let _c = "long:";
 
 let _d = true;
 while (_d) {
-  let _e = prompt('enter something');
+  let _e = prompt("enter something");
 
   if (_e === null) {
     _d = false;
   } else if (_e.length < 5) {
     _a = _a + '\n- "' + _e + '"';
-    alert('short');
+    alert("short");
   } else if (_e.length < 10) {
     _b = _b + '\n- "' + _e + '"';
-    alert('medium');
+    alert("medium");
   } else {
     _c = _c + '\n- "' + _e + '"';
-    alert('long');
+    alert("long");
   }
 }
 
-alert(_a + '\n\n' + _b + '\n\n' + _c);
+alert(_a + "\n\n" + _b + "\n\n" + _c);
